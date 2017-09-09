d75bd9bdef665a2a685904f1b371bd70b01053e4
JuKu <kuenzel.justin@t-online.de>
2017-09-09 20:11:10 +0200

    changed Engine.

--- engine/src/main/java/de/leeksanddragons/engine/Engine.java
+++ engine/src/main/java/de/leeksanddragons/engine/Engine.java
@@ -4,4 +4,8 @@ package de.leeksanddragons.engine;
  * Created by Justin on 09.09.2017.
  */
 public class Engine {
+
+    //current engine build number
+    public static final int ENGINE_VERSION = 1;
+
 }
