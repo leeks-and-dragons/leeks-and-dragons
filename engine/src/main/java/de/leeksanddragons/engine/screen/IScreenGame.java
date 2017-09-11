52fc10e3959dab9cd47c2a8a6c4baf2020de87e9
JuKu <kuenzel.justin@t-online.de>
2017-09-11 12:20:05 +0200

    changed IScreenGame.

--- engine/src/main/java/de/leeksanddragons/engine/screen/IScreenGame.java
+++ engine/src/main/java/de/leeksanddragons/engine/screen/IScreenGame.java
@@ -6,4 +6,10 @@ import de.leeksanddragons.engine.game.IGame;
  * Created by Justin on 11.09.2017.
  */
 public interface IScreenGame extends IGame {
+
+    /**
+    * get instance of screen manager
+    */
+    public <T extends IScreen> ScreenManager<T> getScreenManager ();
+
 }
