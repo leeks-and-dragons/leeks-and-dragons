c3e4478f6bfbe3a3a18fcdeb12ed168bddaa43ac
JuKu <kuenzel.justin@t-online.de>
2017-09-12 01:26:57 +0200

    added BaseScreen.

---
+++ engine/src/main/java/de/leeksanddragons/engine/screen/impl/BaseScreen.java
@@ -0,0 +1,35 @@
+package de.leeksanddragons.engine.screen.impl;
+
+import com.badlogic.gdx.assets.AssetManager;
+import de.leeksanddragons.engine.memory.GameAssetManager;
+import de.leeksanddragons.engine.screen.IScreen;
+import de.leeksanddragons.engine.screen.IScreenGame;
+
+/**
+ * Created by Justin on 06.02.2017.
+ */
+public abstract class BaseScreen implements IScreen {
+
+    protected IScreenGame game;
+    protected GameAssetManager assetManager;
+
+    public final void init(IScreenGame game, GameAssetManager assetManager) {
+        this.game = game;
+        this.assetManager = assetManager;
+
+        this.onInit(game, assetManager);
+    }
+
+    protected abstract void onInit(IScreenGame game, GameAssetManager assetManager);
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+}
