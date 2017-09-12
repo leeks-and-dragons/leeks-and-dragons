c24bcaf897744d1a03f1217bc8b054f966178c4b
JuKu <kuenzel.justin@t-online.de>
2017-09-12 02:00:10 +0200

    added jukusoft logo.

---
+++ game/src/main/java/de/leeksanddragons/game/screen/JuKuSoftIntroScreen.java
@@ -0,0 +1,40 @@
+package de.leeksanddragons.game.screen;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.leeksanddragons.engine.memory.GameAssetManager;
+import de.leeksanddragons.engine.screen.IScreenGame;
+import de.leeksanddragons.engine.screen.impl.BaseScreen;
+import de.leeksanddragons.engine.utils.GameTime;
+
+/**
+ * Created by Justin on 12.09.2017.
+ */
+public class JuKuSoftIntroScreen extends BaseScreen {
+
+    @Override
+    public void init(IScreenGame game, AssetManager assetManager) {
+
+    }
+
+    @Override
+    public void update(IScreenGame game, GameTime time) {
+
+    }
+
+    @Override
+    public void draw(IScreenGame game, GameTime time, SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    protected void onInit(IScreenGame game, GameAssetManager assetManager) {
+
+    }
+
+}
