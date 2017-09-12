cc22360897e155484674fbe8093f8fd4ced0a0d4
JuKu <kuenzel.justin@t-online.de>
2017-09-12 02:41:32 +0200

    added loading screen, see #12 .

--- game/src/main/java/de/leeksanddragons/game/screen/LoadingScreen.java
+++ game/src/main/java/de/leeksanddragons/game/screen/LoadingScreen.java
@@ -2,6 +2,7 @@ package de.leeksanddragons.game.screen;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.leeksanddragons.engine.camera.CameraHelper;
 import de.leeksanddragons.engine.memory.GameAssetManager;
 import de.leeksanddragons.engine.screen.IScreenGame;
 import de.leeksanddragons.engine.screen.impl.BaseScreen;
@@ -12,7 +13,7 @@ import de.leeksanddragons.engine.utils.GameTime;
  */
 public class LoadingScreen extends BaseScreen {
 
-    protected static final String BG_PATH = "";
+    protected static final String BG_PATH = "./data/wallpaper/Loading_Screen.png";
 
     //background image
     protected Texture bgTexture = null;
@@ -20,7 +21,13 @@ public class LoadingScreen extends BaseScreen {
     @Override
     protected void onInit(IScreenGame game, GameAssetManager assetManager) {
         //load background image first
-        //assetManager.load();
+        assetManager.load(BG_PATH, Texture.class);
+
+        //force finish asset loading
+        assetManager.finishLoadingAsset(BG_PATH);
+
+        //get texture
+        this.bgTexture = assetManager.get(BG_PATH);
     }
 
     @Override
@@ -30,12 +37,17 @@ public class LoadingScreen extends BaseScreen {
 
     @Override
     public void draw(IScreenGame game, GameTime time, SpriteBatch batch) {
+        //get camera
+        CameraHelper camera = game.getCameraManager().getMainCamera();
 
+        //draw background
+        batch.draw(this.bgTexture, 0, 0, camera.getViewportWidth(), camera.getViewportHeight());
     }
 
     @Override
     public void dispose() {
-
+        //unload image
+        assetManager.unload(BG_PATH);
     }
 
 }
