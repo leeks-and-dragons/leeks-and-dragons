29d6ce448bf9fc55c9787836a495a47d01fb554d
JuKu <kuenzel.justin@t-online.de>
2017-09-13 22:42:31 +0200

    added support to draw loading bar.

--- game/src/main/java/de/leeksanddragons/game/screen/LoadingScreen.java
+++ game/src/main/java/de/leeksanddragons/game/screen/LoadingScreen.java
@@ -2,8 +2,10 @@ package de.leeksanddragons.game.screen;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import de.leeksanddragons.engine.camera.CameraHelper;
+import de.leeksanddragons.engine.font.BitmapFontFactory;
 import de.leeksanddragons.engine.memory.GameAssetManager;
 import de.leeksanddragons.engine.screen.IScreenGame;
 import de.leeksanddragons.engine.screen.impl.BaseScreen;
@@ -17,6 +19,7 @@ public class LoadingScreen extends BaseScreen {
 
     protected static final String BG_PATH = "./data/wallpaper/Loading_Screen.png";
     protected static final String FG_PATH = "./data/wallpaper/Loading_Screen_layer_2.png";
+    protected static final String PERCENTAGE_FONT_PATH = "./data/font/arial/arial.ttf";//"./data/font/leekling/Leekling.ttf";
 
     //background image
     protected Texture bgTexture = null;
@@ -29,6 +32,9 @@ public class LoadingScreen extends BaseScreen {
     protected final Color barColor = new Color(0xd8d982ff);//#d8d982
 
     protected float percentage = 0;
+
+    protected BitmapFont percentageFont = null;
+    protected static final int PERCENTAGE_FONT_SIZE = 32;
 
     @Override
     protected void onInit(IScreenGame game, GameAssetManager assetManager) {
@@ -43,6 +49,9 @@ public class LoadingScreen extends BaseScreen {
         //get texture
         this.bgTexture = assetManager.get(BG_PATH);
         this.fgTexture = assetManager.get(FG_PATH);
+
+        //create font
+        this.percentageFont = BitmapFontFactory.createFont(PERCENTAGE_FONT_PATH, PERCENTAGE_FONT_SIZE, this.barColor);
     }
 
     @Override
@@ -69,6 +78,9 @@ public class LoadingScreen extends BaseScreen {
 
         //draw bar
         SpriteBatcherUtils.fillRectangle(batch, startX, startY, maxWidth * percentage, barHeight, barColor);
+
+        //draw percentage text
+        this.percentageFont.draw(batch, "" + Math.round((percentage * 100)) + "%", (camera.getViewportWidth() / 2) - 32, 60);
 
         //draw foreground graphics
         batch.draw(this.fgTexture, 0, 0, camera.getViewportWidth(), camera.getViewportHeight());
