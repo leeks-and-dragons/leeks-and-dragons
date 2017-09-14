6c0fe472aa48fd345e0aabe031b5a9536e8a159c
JuKu <kuenzel.justin@t-online.de>
2017-09-14 13:31:36 +0200

    added AssetInfo.

---
+++ engine/src/main/java/de/leeksanddragons/engine/memory/AssetInfo.java
@@ -0,0 +1,30 @@
+package de.leeksanddragons.engine.memory;
+
+/**
+ * Data model for asset loading
+ *
+ * Created by Justin on 14.09.2017.
+ */
+public class AssetInfo {
+
+    public enum TYPE {
+        TEXTURE, UNKNOWN
+    };
+
+    protected String path = "";
+    protected TYPE type = TYPE.UNKNOWN;
+
+    public AssetInfo (String path, TYPE type) {
+        this.path = path;
+        this.type = type;
+    }
+
+    public String getPath () {
+        return this.path;
+    }
+
+    public TYPE getType () {
+        return type;
+    }
+
+}
