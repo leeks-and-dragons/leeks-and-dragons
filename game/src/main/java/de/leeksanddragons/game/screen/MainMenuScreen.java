55c2163acffa22c6435702b8993ae06356d0a874
JuKu <kuenzel.justin@t-online.de>
2017-09-14 19:18:13 +0200

    changed main menu, added background image.

--- game/src/main/java/de/leeksanddragons/game/screen/MainMenuScreen.java
+++ game/src/main/java/de/leeksanddragons/game/screen/MainMenuScreen.java
@@ -1,6 +1,8 @@
 package de.leeksanddragons.game.screen;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.leeksanddragons.engine.camera.CameraHelper;
 import de.leeksanddragons.engine.memory.GameAssetManager;
 import de.leeksanddragons.engine.screen.IScreenGame;
 import de.leeksanddragons.engine.screen.impl.BaseScreen;
@@ -11,9 +13,27 @@ import de.leeksanddragons.engine.utils.GameTime;
  */
 public class MainMenuScreen extends BaseScreen {
 
+    //image paths
+    protected static final String BG_PATH = "./data/wallpaper/Loading_Screen.png";
+    protected static final String FG_PATH = "./data/wallpaper/Loading_Screen_layer_2.png";
+
+    //background image
+    protected Texture bgTexture = null;
+    protected Texture fgTexture = null;
+
     @Override
     protected void onInit(IScreenGame game, GameAssetManager assetManager) {
+        //load background image first
+        assetManager.load(BG_PATH, Texture.class);
+        assetManager.load(FG_PATH, Texture.class);
 
+        //force finish asset loading
+        assetManager.finishLoadingAsset(BG_PATH);
+        assetManager.finishLoadingAsset(FG_PATH);
+
+        //get texture
+        this.bgTexture = assetManager.get(BG_PATH);
+        this.fgTexture = assetManager.get(FG_PATH);
     }
 
     @Override
@@ -23,7 +43,16 @@ public class MainMenuScreen extends BaseScreen {
 
     @Override
     public void draw(IScreenGame game, GameTime time, SpriteBatch batch) {
+        //get camera
+        CameraHelper camera = game.getCameraManager().getMainCamera();
 
+        //draw background
+        batch.draw(this.bgTexture, 0, 0, camera.getViewportWidth(), camera.getViewportHeight());
+
+        //TODO: add code here
+
+        //draw foreground graphics
+        batch.draw(this.fgTexture, 0, 0, camera.getViewportWidth(), camera.getViewportHeight());
     }
 
     @Override
