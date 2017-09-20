ea79008ec37d59bb5d07ccda10e314d72595a361
JuKu <kuenzel.justin@t-online.de>
2017-09-20 14:16:11 +0200

    added new UI widgets.

---
+++ engine/src/main/java/de/leeksanddragons/engine/gui/widgets/ImageWidget.java
@@ -0,0 +1,29 @@
+package de.leeksanddragons.engine.gui.widgets;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import de.leeksanddragons.engine.gui.BaseHUDWidget;
+import de.leeksanddragons.engine.screen.IScreenGame;
+import de.leeksanddragons.engine.utils.GameTime;
+
+/**
+ * Created by Justin on 09.02.2017.
+ */
+public class ImageWidget extends BaseHUDWidget<ImageWidget> {
+
+    protected Texture texture = null;
+
+    public ImageWidget(Texture texture) {
+        this.texture = texture;
+    }
+
+    @Override
+    public void update(IScreenGame game, GameTime time) {
+        //
+    }
+
+    @Override
+    public void drawLayer0(GameTime time, SpriteBatch batch) {
+        batch.draw(this.texture, getX(), getY());
+    }
+}
---
+++ engine/src/main/java/de/leeksanddragons/engine/gui/widgets/WidgetGroup.java
@@ -0,0 +1,72 @@
+package de.leeksanddragons.engine.gui.widgets;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import de.leeksanddragons.engine.gui.BaseHUDWidget;
+import de.leeksanddragons.engine.gui.HUDWidget;
+import de.leeksanddragons.engine.screen.IScreenGame;
+import de.leeksanddragons.engine.utils.GameTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Justin on 09.02.2017.
+ */
+public class WidgetGroup extends BaseHUDWidget<WidgetGroup> {
+
+    /**
+     * list with all child widgets
+     */
+    protected List<HUDWidget> widgetList = new ArrayList<>();
+
+    @Override
+    public void update(IScreenGame game, GameTime time) {
+        this.widgetList.forEach(widget -> {
+            widget.update(game, time);
+        });
+    }
+
+    @Override
+    public void drawLayer0(GameTime time, SpriteBatch batch) {
+        this.widgetList.forEach(widget -> {
+            widget.drawLayer0(time, batch);
+        });
+    }
+
+    @Override
+    public void drawLayer1(GameTime time, ShapeRenderer shapeRenderer) {
+        this.widgetList.forEach(widget -> {
+            widget.drawLayer1(time, shapeRenderer);
+        });
+    }
+
+    @Override
+    public void drawLayer2(GameTime time, SpriteBatch batch) {
+        this.widgetList.forEach(widget -> {
+            widget.drawLayer2(time, batch);
+        });
+    }
+
+    public void addWidget(HUDWidget widget) {
+        // calculate absolute position
+        widget.onMoveGroup(getX(), getY());
+
+        this.widgetList.add(widget);
+    }
+
+    public void removeWidget(HUDWidget widget) {
+        this.widgetList.remove(widget);
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        super.setPosition(x, y);
+
+        // move widgets
+        this.widgetList.forEach(widget -> {
+            widget.onMoveGroup(x, y);
+        });
+    }
+
+}
