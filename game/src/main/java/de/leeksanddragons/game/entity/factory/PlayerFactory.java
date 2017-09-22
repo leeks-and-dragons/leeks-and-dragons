1398d755569e957ffbfa0fafd6ca1534f9c52b99
JuKu <kuenzel.justin@t-online.de>
2017-09-22 23:06:44 +0200

    changed player factory, integrated position component.

--- game/src/main/java/de/leeksanddragons/game/entity/factory/PlayerFactory.java
+++ game/src/main/java/de/leeksanddragons/game/entity/factory/PlayerFactory.java
@@ -2,6 +2,7 @@ package de.leeksanddragons.game.entity.factory;
 
 import de.leeksanddragons.engine.entity.Entity;
 import de.leeksanddragons.engine.entity.EntityManager;
+import de.leeksanddragons.engine.entity.component.PositionComponent;
 import de.leeksanddragons.engine.entity.impl.ECS;
 
 /**
@@ -12,6 +13,9 @@ public class PlayerFactory {
     public static Entity createPlayer (EntityManager ecs, float x, float y) {
         //create new entity
         Entity player = new Entity(ecs);
+
+        //add position component
+        player.addComponent(new PositionComponent(x, y), PositionComponent.class);
 
         return player;
     }
