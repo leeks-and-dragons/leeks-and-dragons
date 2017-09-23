e2d1954583a594f882063c122bac083220d08b75
JuKu <kuenzel.justin@t-online.de>
2017-09-23 18:13:27 +0200

    added utils component.

---
+++ engine/src/main/java/de/leeksanddragons/engine/entity/component/utils/TimedAutoRemoveComponent.java
@@ -0,0 +1,68 @@
+package de.leeksanddragons.engine.entity.component.utils;
+
+import de.leeksanddragons.engine.entity.Entity;
+import de.leeksanddragons.engine.entity.IUpdateComponent;
+import de.leeksanddragons.engine.entity.impl.BaseComponent;
+import de.leeksanddragons.engine.entity.priority.ECSUpdatePriority;
+import de.leeksanddragons.engine.screen.IScreenGame;
+import de.leeksanddragons.engine.utils.GameTime;
+
+/**
+ * Component, which auto removes entity after an given TTL time.
+ *
+ * Created by Justin on 09.03.2017.
+ */
+public class TimedAutoRemoveComponent extends BaseComponent implements IUpdateComponent {
+
+    protected long startTime = 0;
+    protected long ttl = 0;
+
+    public TimedAutoRemoveComponent(long TTL) {
+        this.ttl = TTL;
+    }
+
+    @Override
+    protected void onInit(IScreenGame game, Entity entity) {
+        //
+    }
+
+    @Override
+    public void update(IScreenGame game, GameTime time) {
+        if (startTime == 0) {
+            startTime = time.getTime();
+        }
+
+        // calculate elapsed time
+        long elapsed = time.getTime() - startTime;
+
+        if (elapsed > this.ttl) {
+            // auto remove entity on next gameloop cycle
+            game.runOnUIThread(() -> {
+                // remove entity from ecs
+                this.entity.getEntityComponentSystem().removeEntity(this.entity);
+            });
+        }
+    }
+
+    @Override
+    public ECSUpdatePriority getUpdateOrder() {
+        return ECSUpdatePriority.NORMAL;
+    }
+
+    public long getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getTTL() {
+        return this.ttl;
+    }
+
+    public void setTTL(long ttl) {
+        this.ttl = ttl;
+    }
+
+}
